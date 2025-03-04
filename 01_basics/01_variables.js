15c146b0204433e91355197e15cfbc7d62b92b64
Viratx11 <147901560+Viratx11@users.noreply.github.com>
2025-03-04 11:01:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accid = 144553
+let accmail = "hitesh@google.com"
+var accpass = "12345"
+acccity = "Jaipur"
+let accstate;
+
+// accountId = 2 // not allowed
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+console.log(accid);
+console.table([accid, accmail, accpass, acccity, accstate])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
