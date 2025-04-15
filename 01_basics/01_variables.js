a92a3bbad114c1abb21b37e3266aa2c99a0029e8
Viratx11 <147901560+Viratx11@users.noreply.github.com>
2025-04-15 09:44:21 +0545

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,11 +4,11 @@ var accpass = "12345"
 acccity = "Jaipur"
 let accstate;
 
-// accountId = 2 // not allowed
+// accId = 2 // not allowed
 
-// accountEmail = "hc@hc.com"
-// accountPassword = "21212121"
-// accountCity = "Bengaluru"
+// accEmail = "hc@hc.com"
+// accpass = "21212121"
+// acccity = "Bengaluru"
 
 console.log(accid);
 console.table([accid, accmail, accpass, acccity, accstate])
